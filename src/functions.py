df667f1c0d79f18bbf837af9ae8e56f5de754b6d
bradleywjenks <b.jenks21@imperial.ac.uk>
2024-02-27 19:55:34 +0000

    updated plotting code

--- src/functions.py
+++ src/functions.py
@@ -201,15 +201,10 @@ def load_network_data(inp_file):
     return wdn
 
 
-
-
-
-
 """
-Plot network function
+Plot network layout
 """ 
-
-def plot_network(wdn, plot_type='layout', prv_nodes=None, afv_nodes=None, dbv_nodes=None, iv_nodes=None, sensor_nodes=None, vals_df=None, t=None, legend_labels=None, sensor_labels=False):
+def plot_network_layout(wdn, plot_type='layout', prv_nodes=None, afv_nodes=None, dbv_nodes=None, iv_nodes=None, sensor_nodes=None, vals_df=None, t=None, legend_labels=None, sensor_labels=False):
 
     # unload data
     link_df = wdn.link_df
@@ -250,121 +245,178 @@ def plot_network(wdn, plot_type='layout', prv_nodes=None, afv_nodes=None, dbv_no
                 for _, label in labels_sen.items():
                     label.set_y(label.get_position()[1] + 70)
 
+    # custom legend code
+    if legend_labels is not None:
+        # legend_labels = {'Inlet (source)': 'black', 'PRV': 'black', 'DBV': 'black', 'BV': 'black', 'AFV': 'black', 'Sensor node': 'blue'}
+        legend_handles = [plt.Line2D([0], [0], marker='o' if label == 'Sensor node' else 's' if label == 'Inlet (source)' else '^' if label == 'PRV' else 'd' if label == 'DBV' else 'x' if label == 'IV' else '*' if label == 'AFV' else None, markeredgewidth=2 if label == 'IV' else None, markeredgecolor='black' if label == 'IV' else None, color='white', markerfacecolor=color, markersize=8 if (label == 'Sensor node' or label == 'IV') else 9 if label == 'Inlet (source)' else 10 if (label == 'PRV' or label == 'DBV') else 14 if label == 'AFV' else None, label=label) for label, color in legend_labels.items()]
+        plt.legend(handles=legend_handles, loc='upper right', frameon=False)
 
 
-    elif plot_type == 'hydraulic head':
+
+"""
+Plot network function
+""" 
+
+def plot_network_states(wdn, plot_type='layout', prv_nodes=None, afv_nodes=None, dbv_nodes=None, iv_nodes=None, sensor_nodes=None, vals_df=None, t=None, legend_labels=None, sensor_labels=False):
+
+    # unload data
+    link_df = wdn.link_df
+    node_df = wdn.node_df
+    net_info = wdn.net_info
+    h0_df = wdn.h0_df
+
+    
+    # draw network
+    fig, ax = plt.subplots(figsize=(3.75, 7.25))
+    ax.margins(0.025, 0.025)
+
+
+    if plot_type == 'hydraulic head':
 
         uG = nx.from_pandas_edgelist(link_df, source='node_out', target='node_in')
         pos = {row['node_ID']: (row['xcoord'], row['ycoord']) for _, row in node_df.iterrows()}
-        
-        # create dictionary from dataframe to match node IDs
-        vals_df = vals.set_index('node_ID')[f'h_{t}']
-        h0_df = h0_df.set_index('node_ID')[f'h0_{t}']
 
-        junction_vals = [vals_df[node] for node in net_info['junction_names']]
-        reservoir_vals = [h0_df[node] for node in net_info['reservoir_names']]
-        node_vals_all = junction_vals + reservoir_vals
-
-        # color scaling
-        min_val = min(node_vals_all)
-        max_val = max(node_vals_all)
-
-        # plot hydraulic heads
-        cmap = cm.get_cmap('RdYlBu')
-        nx.draw(uG, pos, nodelist=net_info['junction_names'], node_size=20, node_shape='o', node_color=junction_vals, cmap=cmap, vmin=min_val, vmax=max_val, ax=ax)
-        nx.draw_networkx_nodes(uG, pos, nodelist=net_info['reservoir_names'], node_size=100, node_shape='s', node_color=reservoir_vals, cmap=cmap, vmin=min_val, vmax=max_val) 
-        if prv_nodes is not None:
-            nx.draw_networkx_nodes(uG, pos, nodelist=prv_nodes, node_size=100, node_shape='d', node_color='black') # draw pcv nodes (downstream node)
+        cmap = cm.get_cmap('RdYlGn')
+        norm = plt.Normalize(vmin=vals_df.iloc[:, t].min(), vmax=vals_df.iloc[:, t].max())
+        node_colors = cmap(norm(vals_df.iloc[:, t]))
+        nx.draw(uG, pos, nodelist=vals_df.index, node_size=30, node_shape='o', alpha=0.85, linewidths=0, node_color=node_colors, cmap=cmap, edge_color='grey', ax=ax)
 
         # create a color bar
-        sm = plt.cm.ScalarMappable(cmap=cmap)
-        sm.set_array(node_vals_all)
-        colorbar = plt.colorbar(sm)
+        sm = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
+        sm.set_array(vals_df.iloc[:, t])
+        colorbar = plt.colorbar(sm, orientation='horizontal', pad=-0.025, shrink=0.8)
         colorbar.set_label('Hydraulic head [m]', fontsize=12)
+        # colorbar.set_ticks(colorbar_ticks[0])
+        # colorbar.set_ticklabels(colorbar_ticks[1], fontsize=11)
 
     elif plot_type == 'pressure head':
 
         uG = nx.from_pandas_edgelist(link_df, source='node_out', target='node_in')
         pos = {row['node_ID']: (row['xcoord'], row['ycoord']) for _, row in node_df.iterrows()}
-        
-        # create dictionary from dataframe to match node IDs
-        vals_df = vals.set_index('node_ID')[f'h_{t}']
-        h0_df = h0_df.set_index('node_ID')[f'h0_{t}']
 
-        junction_vals = [vals_df[node] - node_df.loc[node_df['node_ID'] == node, 'elev'].to_numpy()[0] for node in net_info['junction_names']]
-        reservoir_vals = [0 for node in net_info['reservoir_names']]
-        node_vals_all = junction_vals + reservoir_vals
-
-        # color scaling
-        min_val = min(node_vals_all)
-        max_val = max(node_vals_all)
-
-        # plot pressure heads
-        cmap = cm.get_cmap('RdYlBu')
-        nx.draw(uG, pos, nodelist=net_info['junction_names'], node_size=20, node_shape='o', node_color=junction_vals, cmap=cmap, vmin=min_val, vmax=max_val, ax=ax)
-        nx.draw_networkx_nodes(uG, pos, nodelist=net_info['reservoir_names'], node_size=100, node_shape='s', node_color=reservoir_vals, cmap=cmap, vmin=min_val, vmax=max_val) 
-        if prv_nodes is not None:
-            nx.draw_networkx_nodes(uG, pos, nodelist=prv_nodes, node_size=100, node_shape='d', node_color='black') # draw pcv nodes (downstream node)
+        cmap = cm.get_cmap('RdYlGn')
+        norm = plt.Normalize(vmin=vals_df.iloc[:, t].min(), vmax=vals_df.iloc[:, t].max())
+        node_colors = cmap(norm(vals_df.iloc[:, t]))
+        nx.draw(uG, pos, nodelist=vals_df.index, node_size=30, node_shape='o', alpha=0.85, linewidths=0, node_color=node_colors, cmap=cmap, edge_color='grey', ax=ax)
 
         # create a color bar
-        sm = plt.cm.ScalarMappable(cmap=cmap)
-        sm.set_array(node_vals_all)
-        colorbar = plt.colorbar(sm)
+        sm = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
+        sm.set_array(vals_df.iloc[:, t])
+        colorbar = plt.colorbar(sm, orientation='horizontal', pad=-0.025, shrink=0.8)
         colorbar.set_label('Pressure head [m]', fontsize=12)
+        # colorbar.set_ticks(colorbar_ticks[0])
+        # colorbar.set_ticklabels(colorbar_ticks[1], fontsize=11)
+
 
     elif plot_type == 'disinfectant':
 
-            uG = nx.from_pandas_edgelist(link_df, source='node_out', target='node_in')
-            pos = {row['node_ID']: (row['xcoord'], row['ycoord']) for _, row in node_df.iterrows()}
+        uG = nx.from_pandas_edgelist(link_df, source='node_out', target='node_in')
+        pos = {row['node_ID']: (row['xcoord'], row['ycoord']) for _, row in node_df.iterrows()}
 
-            cmap = cm.get_cmap('RdYlBu')
-            norm = plt.Normalize(vmin=vals_df.iloc[:, t].min(), vmax=vals_df.iloc[:, t].max())
-            node_colors = cmap(norm(vals_df.iloc[:, t]))
-            nx.draw(uG, pos, nodelist=vals_df.index, node_size=30, node_shape='o', alpha=0.85, linewidths=0, node_color=node_colors, cmap=cmap, edge_color='grey', ax=ax)
-            nx.draw_networkx_nodes(uG, pos, nodelist=sensor_nodes, node_size=80, node_shape='o', node_color='black', edgecolors='white') # draw sensor nodes
+        cmap = cm.get_cmap('RdYlBu')
+        norm = plt.Normalize(vmin=vals_df.iloc[:, t].min(), vmax=vals_df.iloc[:, t].max())
+        node_colors = cmap(norm(vals_df.iloc[:, t]))
+        nx.draw(uG, pos, nodelist=vals_df.index, node_size=30, node_shape='o', alpha=0.85, linewidths=0, node_color=node_colors, cmap=cmap, edge_color='grey', ax=ax)
 
-            # create a color bar
-            sm = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
-            sm.set_array(vals_df.iloc[:, t])
-            colorbar = plt.colorbar(sm)
-            colorbar.set_label('Disinfectant residual [mg/L]', fontsize=12)
-            # colorbar.set_ticks(colorbar_ticks[0])
-            # colorbar.set_ticklabels(colorbar_ticks[1], fontsize=11)
+        # create a color bar
+        sm = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
+        sm.set_array(vals_df.iloc[:, t])
+        colorbar = plt.colorbar(sm, orientation='horizontal', pad=-0.025, shrink=0.8)
+        colorbar.set_label('Disinfectant residual [mg/L]', fontsize=12)
+        # colorbar.set_ticks(colorbar_ticks[0])
+        # colorbar.set_ticklabels(colorbar_ticks[1], fontsize=11)
 
 
     elif plot_type == 'flow':
 
         edge_df = link_df[['link_ID', 'node_out', 'node_in']]
         edge_df.set_index('link_ID', inplace=True)
-        vals_df = vals.set_index('link_ID')[f'q_{t}']
-        vals_df = abs(vals_df) * 1000
-        edge_df = edge_df.join(vals_df)
 
-        uG = nx.from_pandas_edgelist(edge_df, source='node_out', target='node_in', edge_attr=f'q_{t}')
+        vals = abs(vals_df.iloc[:, t])
+        edge_df = edge_df.join(vals)
+
+        uG = nx.from_pandas_edgelist(edge_df, source='node_out', target='node_in', edge_attr=t/4)
         pos = {row['node_ID']: (row['xcoord'], row['ycoord']) for _, row in node_df.iterrows()}
 
-        # Define colormap
-        cmap = cm.get_cmap('RdYlBu')
+        # make custom colormap
+        min_val, max_val = 0.1,1.0
+        n = 10
+        orig_cmap = cm.Blues
+        colors = orig_cmap(np.linspace(min_val, max_val, n))
+        cmap = mcolors.LinearSegmentedColormap.from_list("mycmap", colors)
 
-        edge_values = nx.get_edge_attributes(uG, f'q_{t}')
+        edge_values = nx.get_edge_attributes(uG, t/4)
+        edge_values = list(edge_values.values())
+
+        # Define colormap
+        # colorbar_ticks = (np.arange(0, 9, 2), [str(int(x)) for x in np.arange(0, 7, 2)] + [r"$\geq 8$"])
+        # cmap = cm.get_cmap('jet_r')
+        # bounds = [0, 1, 4, 6]
+        clims = (0, 8)
+
+        edge_values = nx.get_edge_attributes(uG, t/4)
         edge_values = list(edge_values.values())
 
         # color scaling
-        norm = plt.Normalize(min(edge_values), max(edge_values))
+        # norm = mcolors.BoundaryNorm(bounds, cmap.N)
+        norm = plt.Normalize(vmin=clims[0], vmax=clims[1])
         edge_colors = cmap(norm(edge_values))
 
-
         nx.draw(uG, pos, node_size=0, node_shape='o', node_color='black', ax=ax)
-        nx.draw_networkx_nodes(uG, pos, nodelist=net_info['reservoir_names'], node_size=100, node_shape='s', node_color='black') 
-        nx.draw_networkx_edges(uG, pos, edge_color=edge_colors, width=2) 
-        if prv_nodes is not None:
-            nx.draw_networkx_nodes(uG, pos, nodelist=prv_nodes, node_size=100, node_shape='d', node_color='black') # draw pcv nodes (downstream node)
+        nx.draw_networkx_edges(uG, pos, edge_color=edge_colors, width=1.5) 
 
         # create a color bar
+        cbar_title = 'Flow rate [L/s]'
         sm = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
         sm.set_array(edge_values)
-        colorbar = plt.colorbar(sm)
-        colorbar.set_label('Flow [L/s]', fontsize=12)       
+        colorbar = plt.colorbar(sm, orientation='horizontal', pad=-0.025, shrink=0.8, extend='max', extendfrac=0.075)
+        colorbar.set_label(cbar_title, fontsize=12)
+        # colorbar.set_ticks(colorbar_ticks[0])
+        # colorbar.set_ticklabels(colorbar_ticks[1], fontsize=11)
+
+
+    elif plot_type == 'velocity':
+
+        edge_df = link_df[['link_ID', 'node_out', 'node_in']]
+        edge_df.set_index('link_ID', inplace=True)
+
+        vals = abs(vals_df.iloc[:, t])
+        edge_df = edge_df.join(vals)
+
+        uG = nx.from_pandas_edgelist(edge_df, source='node_out', target='node_in', edge_attr=t/4)
+        pos = {row['node_ID']: (row['xcoord'], row['ycoord']) for _, row in node_df.iterrows()}
+
+        edge_values = nx.get_edge_attributes(uG, t/4)
+        edge_values = list(edge_values.values())
+
+        # Define colormap
+        cmap = mcolors.ListedColormap(['red', 'yellow', 'cyan', 'blue'])
+        # colorbar_ticks = (np.arange(0, 9, 2), [str(int(x)) for x in np.arange(0, 7, 2)] + [r"$\geq 8$"])
+        # cmap = cm.get_cmap('jet_r')
+        bounds = [0, 0.1, 0.2, 0.3, 0.4]
+        # clims = (0, 6)
+
+        edge_values = nx.get_edge_attributes(uG, t/4)
+        edge_values = list(edge_values.values())
+
+        # color scaling
+        norm = mcolors.BoundaryNorm(bounds, cmap.N)
+        # norm = plt.Normalize(vmin=clims[0], vmax=clims[1])
+        edge_colors = cmap(norm(edge_values))
+
+        nx.draw(uG, pos, node_size=0, node_shape='o', node_color='black', ax=ax)
+        nx.draw_networkx_edges(uG, pos, edge_color=edge_colors, width=1.5) 
+
+        # create a color bar
+        cbar_title = 'Flow velocity [m/s]'
+        sm = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
+        sm.set_array(edge_values)
+        colorbar = plt.colorbar(sm, orientation='horizontal', pad=-0.025, shrink=0.8, extend='max', extendfrac=0.075)
+        colorbar.set_label(cbar_title, fontsize=12)
+        # colorbar.set_ticks(colorbar_ticks[0])
+        # colorbar.set_ticklabels(colorbar_ticks[1], fontsize=11)
+
+        
 
     
     # # reservoir labels
@@ -380,12 +432,40 @@ def plot_network(wdn, plot_type='layout', prv_nodes=None, afv_nodes=None, dbv_no
     #     for _, label in labels_2.items():
     #         label.set_y(label.get_position()[1] + 80)
 
+    # control node labels
+    nx.draw_networkx_nodes(uG, pos, nodelist=net_info['reservoir_names'], node_size=100, node_shape='s', node_color='black') # draw reservoirs
+
+    if prv_nodes is not None:
+        nx.draw_networkx_nodes(uG, pos, nodelist=prv_nodes, node_size=120, node_shape='^', node_color='black', edgecolors='white')
+
+    if iv_nodes is not None:
+        nx.draw_networkx_nodes(uG, pos, nodelist=iv_nodes, node_size=65, node_shape='x', linewidths=2, node_color='black', edgecolors='white')
+    
+    if dbv_nodes is not None:
+        nx.draw_networkx_nodes(uG, pos, nodelist=dbv_nodes, node_size=120, node_shape='d', node_color='black', edgecolors='white')
+    
+    if afv_nodes is not None:
+        nx.draw_networkx_nodes(uG, pos, nodelist=afv_nodes, node_size=200, node_shape='*', node_color='black', edgecolors='white')
+
+    if sensor_nodes is not None:
+        nx.draw_networkx_nodes(uG, pos, nodelist=sensor_nodes, node_size=80, node_shape='o', node_color='black', edgecolors='white')
+
+        if sensor_labels:
+            sensor_labels = {node: str(idx+1) for (idx, node) in enumerate(sensor_nodes)}
+            labels_sen = nx.draw_networkx_labels(uG, pos, sensor_labels, font_size=12, verticalalignment='bottom')
+            for _, label in labels_sen.items():
+                label.set_y(label.get_position()[1] + 70)
+
 
     # custom legend code
     if legend_labels is not None:
         # legend_labels = {'Inlet (source)': 'black', 'PRV': 'black', 'DBV': 'black', 'BV': 'black', 'AFV': 'black', 'Sensor node': 'blue'}
-        legend_handles = [plt.Line2D([0], [0], marker='o' if label == 'Sensor node' else 's' if label == 'Inlet (source)' else '^' if label == 'PRV' else 'd' if label == 'DBV' else 'x' if label == 'IV' else '*' if label == 'AFV' else None, markeredgewidth=2 if label == 'IV' else None, markeredgecolor='black' if label == 'IV' else None, color='white', markerfacecolor=color, markersize=8 if (label == 'Sensor node' or label == 'IV') else 9 if label == 'Inlet (source)' else 10 if (label == 'PRV' or label == 'DBV') else 14 if label == 'AFV' else None, label=label) for label, color in legend_labels.items()]
-        plt.legend(handles=legend_handles, loc='upper right', frameon=False)
+        legend_handles = [plt.Line2D([0], [0], marker='o' if label == 'Sensor' else 's' if label == 'Inlet' else '^' if label == 'PRV' else 'd' if label == 'DBV' else 'x' if label == 'IV' else '*' if label == 'AFV' else None, markeredgewidth=2 if label == 'IV' else None, markeredgecolor='black' if label == 'IV' else None, color='white', markerfacecolor=color, markersize=8 if (label == 'Sensor' or label == 'IV') else 9 if label == 'Inlet' else 10 if (label == 'PRV' or label == 'DBV') else 14 if label == 'AFV' else None, label=label) for label, color in legend_labels.items()]
+
+    leg = plt.legend(handles=legend_handles, loc='upper right', frameon=True, borderpad=0.75)
+    leg.get_frame().set_edgecolor('black')
+    leg.get_frame().set_linewidth(0.5)
+
 
 
 
